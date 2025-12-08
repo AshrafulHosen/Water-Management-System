3e06823e1868e7e330e1e6204f4f8d97ab2fb89a
AsharfulHosen <ashrafulaliff7@gamil.com>
2025-12-08 21:30:51 +0600

    some changes in LoginController

--- src/main/java/com/example/watermanagementsystem/controllers/LoginController.java
+++ src/main/java/com/example/watermanagementsystem/controllers/LoginController.java
@@ -1,17 +1,22 @@
 package com.example.watermanagementsystem.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class LoginController {
     @FXML
-    protected void handleUserLoginButton() {
+    private Label messageLabel;
 
+    @FXML
+    protected void handleUserLoginButton() {
+        messageLabel.setText("Enter your username and password");
     }
     @FXML
     protected void handleAdminLoginButton() {
-
+        messageLabel.setText("Admin Login");
     }
     @FXML
     protected void handleCreateAccountButton() {
+        messageLabel.setText("Create Account");
     }
 }

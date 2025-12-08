5bed0b4ca6856b5b9134b60282306d154459101f
AsharfulHosen <ashrafulaliff7@gamil.com>
2025-12-08 21:10:40 +0600

    create login fxml

---
+++ src/main/java/com/example/watermanagementsystem/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.watermanagementsystem.controllers;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+    @FXML
+    protected void handleUserLoginButton() {
+
+    }
+    @FXML
+    protected void handleAdminLoginButton() {
+
+    }
+    @FXML
+    protected void handleCreateAccountButton() {
+    }
+}
